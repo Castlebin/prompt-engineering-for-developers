2675a67f9b6dd7cdcf145de82093cc9c9b8b5dfd
Castlebin <Castlebin@gmail.com>
2025-04-12 22:23:48 +0800

    测试 阿里百炼平台的 大模型api （OpenAI 方式调用）

---
+++ code/C1/test_dashscope.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+# 读取本地/项目的环境变量。
+# find_dotenv()寻找并定位.env文件的路径
+# load_dotenv()读取该.env文件，并将其中的环境变量加载到当前的运行环境中
+# 如果你设置的是全局的环境变量，这行代码则没有任何作用。
+load_dotenv(find_dotenv())
+
+client = OpenAI(
+    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+completion = client.chat.completions.create(
+    model="qwen-plus",  # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    messages=[
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': '你是谁？'}],
+)
+
+print(completion.model_dump_json())
